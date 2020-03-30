1e0de22a58a7a197e5d24017903b5b1abb748489
IgoreKMaN <y1876@mail.ru>
2020-03-30 21:42:40 +0300

    vlad2

--- script/script.js
+++ script/script.js
@@ -1,2 +1,14 @@
-let elem = document.getElementsByClassName("in__total")[0].innerHTML;
+//let elem = document.getElementsByClassName("in__total")[0].innerHTML;
+
+let inTotal = document.getElementById("in__total").innerHTML;
+let collected = document.getElementById("collected").innerHTML;
+let percentageTotal = document.getElementById("percentage__total");
+
+let percent = 0;
+
+percent = (collected * 100 / inTotal).toFixed(1);
+
+percentageTotal.innerText = percent;
+
+
 
