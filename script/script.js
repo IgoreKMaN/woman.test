cf92fd0a657c32fbb03ba18d38ebac60ea85fbe4
IgoreKMaN <y1876@mail.ru>
2020-03-30 22:49:23 +0300

    vlad3

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ let percentageTotal = document.getElementById("percentage__total");
 
 let percent = 0;
 
-percent = (collected * 100 / inTotal).toFixed(1);
+percent = (collected * 100 / inTotal).toFixed(1) + "%";
 
 percentageTotal.innerText = percent;
 
