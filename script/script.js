e1d763d198105787d953a5d2e6456548a317d1fd
IgoreKMaN <y1876@mail.ru>
2020-03-31 15:37:56 +0300

    vlad5

--- script/script.js
+++ script/script.js
@@ -3,12 +3,24 @@
 let inTotal = document.getElementById("in__total").innerHTML;
 let collected = document.getElementById("collected").innerHTML;
 let percentageTotal = document.getElementById("percentage__total");
-
 let percent = 0;
 
 percent = (collected * 100 / inTotal).toFixed(1) + "%";
-
 percentageTotal.innerText = percent;
 
+
+
+
+$(".arrow__down").click(function(){
+    if ($(".arrow__down").hasClass('arrow__reverce')){
+        $(".arrow__down").removeClass("arrow__reverce");
+        $(".all__document").removeClass("document__open");
+    }
+    else{
+        $(".arrow__down").addClass("arrow__reverce");
+        $(".all__document").addClass("document__open");
+ 
+    }
+});
 
 
