89895658ea027a4261ae0427ff64f5fde908fade
IgoreKMaN <y1876@mail.ru>
2020-04-22 16:47:39 +0300

    modal

--- script/script.js
+++ script/script.js
@@ -45,8 +45,48 @@ percent = (collected * 100 / inTotal).toFixed(1) + "%";
 percentageTotal.innerText = percent;
     
     
+    
+    
+    
+    
+    
+  
+   $(".leaflets-img").click(function(){
+       $("body").css({"overflow":"hidden"});
+        
+       let img = $(this);
+       let src = img.attr("src");
+       $(".leaflets").append("<div id='myModal' class='modal'>" +
+            "<div class='modal-content'>" +
+                "<div class='modal-header'>" +
+                    "<span class='close'>&times;</span>" +
+                "</div>" +
+                "<div class='modal-body'>" +
+                    "<img src='" + src + "' alt=''>" +
+                "</div>" +
+            "</div>" +
+        "</div>");
+       $("#myModal").fadeIn(800);
+       
+       $(".close").click(function(){
+        $("#myModal").fadeOut(800);
+           setTimeout(function(){
+              $("#myModal").remove(); 
+           }, 800);
+        $("body").css({"overflow":"visible"});    
+   });
+       
+   });
+    
+    
+    
 });
 
 
 
 
+
+
+
+
+ 
